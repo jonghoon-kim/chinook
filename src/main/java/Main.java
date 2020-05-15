e3fc6306e67be4107889763ca28d7fda21c02313
cplkimth <cplkimth@naver.com>
2020-05-15 15:19:50 +0900

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,10 @@ import java.util.ArrayList;
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-        Album album = AlbumDao.getInstance().getByKey(2);
-        ArrayList<Album> albums = AlbumDao.getInstance().getAll();
-        ArrayList<Album> albums2 = AlbumDao.getInstance().getByArtistId(3);
-        int albumCount = AlbumDao.getInstance().getCount();
-
-        Album newAlbum = new Album();
-        newAlbum.setTitle("new album");
-        newAlbum.setArtistId(3);
-        boolean inserted = AlbumDao.getInstance().insert(newAlbum);
-        System.out.println(inserted);
-        int maxAlbumId = AlbumDao.getInstance().getMaxAlbumId();
-        System.out.println(maxAlbumId); // 새로 추가된 albumId
+        var album = AlbumDao.getInstance().getByKey(2);
+        System.out.println(album);
+//        ArrayList<Album> albums = AlbumDao.getInstance().getAll();
+//        ArrayList<Album> albums2 = AlbumDao.getInstance().getByArtistId(3);
+//        int albumCount = AlbumDao.getInstance().getCount();
     }
-
-
 }
